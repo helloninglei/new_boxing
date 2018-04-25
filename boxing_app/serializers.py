8b3c3b9fa79322eb40a87bfe021576e624c3d15f
Lerry <lvdachao@tuotiansudai.com>
2018-04-25 19:07:18 +0800

    message

--- boxing_app/serializers.py
+++ boxing_app/serializers.py
@@ -2,14 +2,19 @@
 
 from rest_framework import serializers
 from biz import models
+from django.forms.models import model_to_dict
+
+
+class MessageUserField(serializers.RelatedField):
+    def to_representation(self, user):
+        return model_to_dict(user.user_profile, fields=('nick_name', 'avatar'))
 
 
 class MessageSerializer(serializers.ModelSerializer):
     images = serializers.ListField(child=serializers.URLField())
+    user = MessageUserField(read_only=True)
 
     class Meta:
         model = models.Message
         fields = ['id', 'content', 'images', 'video', 'created_time', 'user']
-        read_only_fields = ['user']
         extra_kwargs = {'images': {'required': False}, 'video': {'required': False}}
-
