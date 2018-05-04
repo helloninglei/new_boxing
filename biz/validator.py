cea1bfdab1be135d85e177c68b248de197201cdc
ninglei <zhangninglei@tuotiansudai.com>
2018-05-04 11:16:57 +0800

    fix validate_identity_number

--- biz/validator.py
+++ biz/validator.py
@@ -5,15 +5,16 @@ import re
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+mobile_regex = re.compile(r'^1\d{10}$')
+identity_number_regex = re.compile(r'^\d{17}[xX0-9]$')
+
 
 def validate_mobile(value):
-    mobile_regex = re.compile(r'^1\d{10}$')
     if not mobile_regex.match(value):
         raise ValidationError(
             _('%(value)s is not an mobile'), params={'value': value},
         )
 
 def validate_identity_number(value):
-    identity_number_regex = re.compile(r'^\d{18}$')
     if identity_number_regex.match(value) == None:
         raise ValidationError( "身份证号码格式错误！")
