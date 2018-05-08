d6e93165167dbe9a513fa99c7fc70e098e59d47f
fzw <fangzhaowei@tuotiansudai.com>
2018-05-08 20:30:51 +0800

    add ut

---
+++ boxing_app/tests/test_send_verify_code.py
@@ -0,0 +1,20 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from biz import redis_client
+from biz.redis_const import SEND_VERIFY_CODE
+
+
+class SendVerifyCodeVerify(APITestCase):
+    def setUp(self):
+        self.client = self.client_class()
+
+    def test_send_verify_code(self):
+        redis_client.delete(SEND_VERIFY_CODE.format(mobile="13300000000"))
+        response = self.client.post(path='/verify_code', data={"mobile": "13300000000"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.client.post(path='/verify_code', data={"mobile": "13300000000"})
+        self.assertEqual(response.data['message'][0], "图形验证码错误！")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        redis_client.delete(SEND_VERIFY_CODE.format(mobile="13300000000"))
+        response = self.client.post(path="/verify_code", data={"mobile": "13300000000"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
