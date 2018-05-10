c136cbcc9aea375c7a0bd1e0d1f2e7b92781b385
Lerry <lvdachao@tuotiansudai.com>
2018-05-10 14:12:19 +0800

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-
-
-docker run -d -p 8001:8000 --name new_boxing_app -it new_boxing_image -v .:/work boxing_app/uwsgi.ini:/etc/uwsgi.ini /bin/bash /work/run.sh
-docker run -d -p 8002:8000 --name new_boxing_console -it new_boxing_image -v .:/work boxing_console:/etc/uwsgi.ini /bin/bash /work/run.sh
+docker run -d -p 8001:8000 --name new_boxing_app -v .:/work boxing_app/uwsgi.ini:/etc/uwsgi.ini /bin/bash -it new_boxing_image /work/run.sh
+docker run -d -p 8002:8000 --name new_boxing_console -v .:/work boxing_console:/etc/uwsgi.ini -it new_boxing_image /bin/bash /work/run.sh
