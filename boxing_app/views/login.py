2d234a2acfcdae504d918ea5a14c6790377256e7
fzw <fangzhaowei@tuotiansudai.com>
2018-05-11 17:39:24 +0800

    del not use import code

--- boxing_app/views/login.py
+++ boxing_app/views/login.py
@@ -1,4 +1,4 @@
-from rest_framework.authtoken.views import ObtainAuthToken, obtain_auth_token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions, status
 from rest_framework.response import Response
