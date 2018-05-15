9e233d24b3e1c42e891cccebb986cdf2d30555a9
ninglei <zhangninglei@tuotiansudai.com>
2018-05-15 14:47:37 +0800

    club list and test

--- boxing_console/tests/test_club.py
+++ boxing_console/tests/test_club.py
@@ -78,3 +78,55 @@ class BoxingClubTestCase(TestCase):
         # 判断redis记录已删除
         redis_location_record = redis_client.get_boxing_club_location(create_res.data['id'])
         self.assertIsNone(redis_location_record[0])
+
+    def test_club_list(self):
+        data1 = {
+            "club_name": "拳王01",
+            "address": "丰台区",
+            "longitude": 111.111111,
+            "latitude": 11.111111,
+            "phone": "11111111111",
+            "opening_hours": "10:00--20:00",
+            "images": ["www.baidu01.com", "www.sina01.com.cn"],
+            "introduction": "最牛逼的拳馆"
+        }
+        data2 = {
+            "club_name": "拳王02",
+            "address": "朝阳区",
+            "longitude": 112.111111,
+            "latitude": 12.111111,
+            "phone": "11111111112",
+            "opening_hours": "10:00--20:00",
+            "images": ["www.baidu02.com", "www.sina02.com.cn"],
+            "introduction": "最善良的拳馆"
+        }
+        data3 = {
+            "club_name": "拳王03",
+            "address": "海淀区",
+            "longitude": 113.111111,
+            "latitude": 13.111111,
+            "phone": "11111111113",
+            "opening_hours": "10:00--20:00",
+            "images": ["www.baidu03.com", "www.sina03.com.cn"],
+            "introduction": "最火爆的拳馆"
+        }
+        BoxingClub.objects.bulk_create([BoxingClub(**data) for data in (data1, data2, data3)])
+
+        # 获取拳馆列表，并验证数据
+        list_res = self.client.get('/club')
+        self.assertEqual(list_res.status_code, status.HTTP_200_OK)
+        self.assertEqual(list_res.data['count'], 3)
+        for key in data1:
+            if key in ("longitude", "latitude"):
+                self.assertEqual(str(data1[key]), list_res.data['results'][0].get(key))
+            else:
+                self.assertEqual(data1[key], list_res.data['results'][0].get(key))
+
+        # 通过拳馆名进行搜索,并验证结果
+        search_res = self.client.get('/club', data={'search': data2['club_name']})
+        self.assertEqual(search_res.data['count'], 1)
+        for key in data2:
+            if key in ("longitude", "latitude"):
+                self.assertEqual(str(data2[key]), search_res.data['results'][0].get(key))
+            else:
+                self.assertEqual(data2[key], search_res.data['results'][0].get(key))
