9b44bbd265646efd4e7ec35371e595cf7b59e54d
Lerry <lvdachao@tuotiansudai.com>
2018-05-16 19:29:38 +0800

    update pay service

--- biz/services/pay_service.py
+++ biz/services/pay_service.py
@@ -89,6 +89,4 @@ class PayService:
 
     @classmethod
     def success_callback(cls, data):
-        order = PayOrder.objects.get(out_trade_no=data['out_trade_no'])
-        order.status = PAYMENT_STATUS_PAID
-        order.save()
+        PayOrder.objects.get(out_trade_no=data['out_trade_no']).update(status=PAYMENT_STATUS_PAID)
