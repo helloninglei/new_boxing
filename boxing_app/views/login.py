1c049395e2d8eb2c54f8f669342dd9ac3a4cf3da
fzw <fangzhaowei@tuotiansudai.com>
2018-05-16 19:05:38 +0800

    fix  code

--- boxing_app/views/login.py
+++ boxing_app/views/login.py
@@ -34,5 +34,5 @@ def change_password(request):
     user = request.user
     user.set_password(serializer.validated_data['new_password'])
     user.save()
-    request.user.auth_token.delete()
+    user.auth_token.delete()
     return Response({"message": "ok"}, status=status.HTTP_200_OK)
