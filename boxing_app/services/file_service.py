2f9e7fa56cc0be3cfa0af07c3a74d251e84c5768
Lerry <lvdachao@tuotiansudai.com>
2018-05-17 13:30:25 +0800

    update file upload

--- boxing_app/services/file_service.py
+++ boxing_app/services/file_service.py
@@ -1,22 +1,40 @@
+# coding: utf-8
 import os
-import uuid
-import datetime
-from biz.storages import storage
-
-def save_upload_file(upfile):
-    file_path = _rename(upfile)
-    return storage.base_url + storage.save(file_path, upfile)
+import oss2
+import hashlib
+from urllib.parse import urljoin
+from django.conf import settings
 
 
-def only_save_upload_file(f, file_path=None):
-    if file_path is None:
-        file_path = _rename(f)
-    return storage.base_url + storage.save(file_path, f)
+class OssClient(object):
+    def __init__(self, base_url=None):
+        cfg = settings.OSS_CONFIG
+        self.base_url = base_url
+        self.auth = oss2.Auth(cfg['app_key'], cfg['app_secret'])
+        self.bucket = oss2.Bucket(self.auth, cfg['url'], cfg['bucket'])
+
+    def save(self, file_path, file_data):
+        file_path = urljoin(self.base_url, file_path)
+        self.bucket.put_object(file_path[1:], file_data)
+        return file_path
 
 
-def _rename(f):
-    fn = uuid.uuid4().hex
+storage = OssClient(base_url=settings.UPLOAD_URL_PATH)
+
+
+def save_upload_file(upload_file):
+    file_path = generate_file_name(upload_file)
+    return storage.save(file_path, upload_file)
+
+
+def generate_file_name(f):
+    h = hashlib.sha1()
+    while 1:
+        block = f.read(128 * 1024)
+        if not block:
+            break
+        h.update(block)
+    digest = h.hexdigest()
+    f.seek(0)
     file_name, file_ext = os.path.splitext(f.name)
-    year = str(datetime.datetime.today().year)
-    file_path = year + '/' + fn[0] + '/' + fn[0:2] + '/' + fn + file_ext
-    return file_path
+    return digest[:2] + '/' + digest[2:4] + '/' + digest[4:] + file_ext
