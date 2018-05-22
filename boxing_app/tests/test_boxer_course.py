61deae8e3f3dfab6ebf761ebac814f27417e6c14
ninglei <zhangninglei@tuotiansudai.com>
2018-05-22 15:52:27 +0800

    fix verify for course

--- boxing_app/tests/test_boxer_course.py
+++ boxing_app/tests/test_boxer_course.py
@@ -29,7 +29,8 @@ class CourseTestCase(APITestCase):
                     "experience": '',
                     "honor_certificate_images": ['http://img1.com', 'http://img2.com', 'http://img3.com'],
                     "competition_video": 'https://baidu.com',
-                    "allowed_course": [constants.BOXER_ALLOWED_COURSES_BOXING, constants.BOXER_ALLOWED_COURSES_MMA]
+                    "allowed_course": [constants.BOXER_ALLOWED_COURSES_BOXING, constants.BOXER_ALLOWED_COURSES_MMA,
+                                       constants.BOXER_ALLOWED_COURSES_THAI_BOXING]
                 }
         self.club_data = {
                     "name": "club01",
@@ -51,37 +52,62 @@ class CourseTestCase(APITestCase):
         course_list_res = self.client1.get('/boxer/course')
         self.assertEqual(len(course_list_res.data['results']), len(self.boxer_data['allowed_course']))
 
-        # 构造修改课程列表数据，并修改课程
+        # 构造修改课程列表数据，并修改课程(is_open为False,不校验数据合法性）
         my_club = BoxingClub.objects.create(**self.club_data)
         course1_data = {
+            'is_open': False,
             'course_id': course_list_res.data['results'][0].get('id'),
             'price': 120,
             'duration': 365,
         }
         course2_data = {
+            'is_open': False,
             'course_id': course_list_res.data['results'][1].get('id'),
-            'price': 120,
+            'price': 121,
             'duration': 366,
+        }
+        course3_data = {
+            'course_id': course_list_res.data['results'][2].get('id'),
+            'is_open': True,
+            'price': 122,
+            'duration': 367,
         }
         update_course_data = {
             "validity": "2018-04-25",
             "club": my_club.pk,
             "course_list": [{**course1_data}, {**course2_data}]}
+
+        # 修改未开通课程，不需要校验数据
         update_res = self.client1.post('/boxer/course', data=json.dumps(update_course_data),
                                        content_type='application/json')
         self.assertEqual(update_res.status_code, status.HTTP_302_FOUND)
 
-        # 获取修改后的课程数据,
+        # 修改开通课程，需要校验数据（数据不合法，校验失败）
+        course3_data['price'] = ''
+        update_course_data["course_list"] = [{**course1_data}, {**course2_data}, {**course3_data}]
+        update_res = self.client1.post('/boxer/course', data=json.dumps(update_course_data),
+                                       content_type='application/json')
+        self.assertEqual(update_res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # 修改开通课程，需要校验数据（数据合法，校验成功）
+        course3_data['price'] = 120
+        update_course_data["course_list"] = [{**course1_data}, {**course2_data}, {**course3_data}]
+        update_res = self.client1.post('/boxer/course', data=json.dumps(update_course_data),
+                                       content_type='application/json')
+        self.assertEqual(update_res.status_code, status.HTTP_302_FOUND)
+
+        # 比较修改后的课程数据与修改数据是否一致
         course_list_res = self.client1.get('/boxer/course')
         course_result = course_list_res.data['results']
         self.assertEqual(len(course_list_res.data['results']), len(self.boxer_data['allowed_course']))
-        self.assertEqual(course_result[0]['id'], course1_data['course_id'])
-        self.assertEqual(course_result[0]['price'], course1_data['price'])
-        self.assertEqual(course_result[0]['duration'], course1_data['duration'])
-        self.assertEqual(course_result[0]['validity'], update_course_data['validity'])
-        self.assertEqual(course_result[0]['club'], update_course_data['club'])
-        self.assertEqual(course_result[0]['validity'], update_course_data['validity'])
-        self.assertEqual(course_result[0]['club_name'], self.club_data['name'])
-        self.assertEqual(course_result[0]['club_address'], self.club_data['address'])
-        self.assertEqual(str(course_result[0]['club_longitude']), str(self.club_data['longitude']))
-        self.assertEqual(str(course_result[0]['club_latitude']), str(self.club_data['latitude']))
+        self.assertTrue(course_result[2]['is_open'])
+        self.assertEqual(course_result[2]['id'], course3_data['course_id'])
+        self.assertEqual(course_result[2]['price'], course3_data['price'])
+        self.assertEqual(course_result[2]['duration'], course3_data['duration'])
+        self.assertEqual(course_result[2]['validity'], update_course_data['validity'])
+        self.assertEqual(course_result[2]['club'], update_course_data['club'])
+        self.assertEqual(course_result[2]['validity'], update_course_data['validity'])
+        self.assertEqual(course_result[2]['club_name'], self.club_data['name'])
+        self.assertEqual(course_result[2]['club_address'], self.club_data['address'])
+        self.assertEqual(str(course_result[2]['club_longitude']), str(self.club_data['longitude']))
+        self.assertEqual(str(course_result[2]['club_latitude']), str(self.club_data['latitude']))
