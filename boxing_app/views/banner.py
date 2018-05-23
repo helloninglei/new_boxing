c40bb58a3a52aa7445a846be16a6d97bde25c3c5
Lerry <lvdachao@tuotiansudai.com>
2018-05-23 16:57:54 +0800

    fix

--- boxing_app/views/banner.py
+++ boxing_app/views/banner.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from biz import models
 from boxing_app.serializers import BannerSerializer
 
 
 class BannerViewSet(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = []
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = BannerSerializer
     queryset = models.Banner.objects.all()
