7395d01d0843a7da2e1cdf58baedb6b9b1eced53
fzw <fangzhaowei@tuotiansudai.com>
2018-05-25 14:24:45 +0800

    add ut

--- boxing_app/serializers.py
+++ boxing_app/serializers.py
@@ -346,6 +346,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     money_balance = serializers.SerializerMethodField()
     user_id = serializers.CharField(source="user.id", read_only=True)
     boxer_status = serializers.CharField(source='user.boxer_identification.authentication_state', read_only=True)
+    identity = serializers.CharField(source="user.identity", read_only=True)
 
     def get_money_balance(self, instance):
         return instance.user.money_balance
