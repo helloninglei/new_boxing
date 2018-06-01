0c4c30b1fdd9637504be7e5973af92004775f5e0
Lerry <lvdachao@tuotiansudai.com>
2018-06-01 12:16:25 +0800

    hot video url

--- boxing_app/views/hot_video.py
+++ boxing_app/views/hot_video.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
-from rest_framework import viewsets, permissions
 from django.db.models import Count, Q
+from django.shortcuts import redirect
+from rest_framework import viewsets, permissions
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes, authentication_classes
 from biz import models
-from biz.constants import PAYMENT_STATUS_WAIT_USE
+from biz.constants import PAYMENT_STATUS_WAIT_USE, HOT_VIDEO_USER_ID
 from boxing_app.serializers import HotVideoSerializer
+
+
+@api_view(['GET'])
+@permission_classes([permissions.AllowAny])
+@authentication_classes([])
+def hot_video_redirect(request):
+    url = reverse('hot-video', kwargs={'user_id': HOT_VIDEO_USER_ID})
+    return redirect(url)
 
 
 class HotVideoViewSet(viewsets.ReadOnlyModelViewSet):
