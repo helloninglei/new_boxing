24b119f6c7ff6ac4169f1e47d9067adeea198701
fzw <fangzhaowei@tuotiansudai.com>
2018-06-01 17:27:58 +0800

    fix bug

--- boxing_app/views/version.py
+++ boxing_app/views/version.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view, permission_classes, authenticati
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from django.conf import settings
+from biz.utils import get_device_platform
 
 version_mapping = {
     'ANDROID': settings.ANDROID_VERSION,
@@ -13,7 +14,7 @@ version_mapping = {
 @permission_classes([permissions.AllowAny])
 @authentication_classes([])
 def version(request):
-    device = request.META.get('HTTP_PLATFORM')
+    device = get_device_platform(request)
     if device is None:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     version_response = version_mapping.get(device.upper())
