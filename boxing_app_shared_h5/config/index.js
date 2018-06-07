a1b019bbcaaa5903931fd8642d49605f11d6fd66
Lerry <lvdachao@tuotiansudai.com>
2018-06-07 18:31:25 +0800

    fix

--- boxing_app_shared_h5/config/index.js
+++ boxing_app_shared_h5/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../../share_h5/index.html'),
     assetsRoot: path.resolve(__dirname, '../../share_h5'),
     assetsSubDirectory: '',
-    assetsPublicPath: '/share',
+    assetsPublicPath: '/share/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
