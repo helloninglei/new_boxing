94c16f4e63f2bb4c7fdfd0bc0b258934d2af7934
Lerry <lvdachao@tuotiansudai.com>
2018-06-07 16:33:20 +0800

    update h5 share path

--- boxing_app_shared_h5/config/index.js
+++ boxing_app_shared_h5/config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../../templates/index.html'),
-    assetsRoot: path.resolve(__dirname, '../../static'),
+    index: path.resolve(__dirname, '../../share_h5/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../share_h5'),
     assetsSubDirectory: '',
     assetsPublicPath: '/static',
     productionSourceMap: true,
