869e4c51fc09702cf0c5c6f32efccbf4564c0b24
Lerry <lvdachao@tuotiansudai.com>
2018-06-08 14:39:33 +0800

    update news redirect

--- boxing_app/views/game_news.py
+++ boxing_app/views/game_news.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from django.shortcuts import redirect
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from django.db.models import Count, F
 from biz import models
+from biz.utils import get_device_platform
 from boxing_app import serializers
+
+h5_base_url = settings.SHARE_H5_BASE_URL
 
 
 class NewsViewSet(viewsets.ReadOnlyModelViewSet):
@@ -12,6 +17,9 @@ class NewsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.GameNews.objects.annotate(comment_count=Count('comments'))
 
     def retrieve(self, request, *args, **kwargs):
-        # TODO  App访问时跳转到网页
-        models.GameNews.objects.filter(pk=self.kwargs['pk']).update(views_count=F('views_count') + 1)
+        news_id = self.kwargs['pk']
+        url = f'{h5_base_url}game_news/{news_id}'
+        if get_device_platform(request):
+            models.GameNews.objects.filter(pk=news_id).update(views_count=F('views_count') + 1)
+            return redirect(url)
         return super().retrieve(request, *args, **kwargs)
