d7e5e898fe2aecd806b56382ecc6b241ba8937c3
ninglei <zhangninglei@tuotiansudai.com>
2018-06-13 08:27:13 +0800

    del boxer location when close club

--- boxing_app/views/course.py
+++ boxing_app/views/course.py
@@ -42,32 +42,35 @@ class BoxerMyCourseViewSet(viewsets.ModelViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def list(self, request, *args, **kwargs):
-        boxer = BoxerIdentification.objects.get(user=self.request.user)
-        course = self.get_queryset().filter(is_open=True).last()
         response = super().list(request, *args, **kwargs)
-        base_info = self.get_base_info(course=course, boxer=boxer)
-        response.data.update(base_info)
+        boxer = BoxerIdentification.objects.get(user=self.request.user)
+        self.get_boxer_base_info(dict=response.data, boxer=boxer)
+        course = self.get_queryset().filter(is_open=True).select_related('club').last()
+        if course:
+            self.get_course_base_info(dict=response.data, course=course)
         return response
 
-    @classmethod
-    def get_base_info(cls, course, boxer):
-        base_info = {}
-        comment_count_and_avg_score = OrderComment.objects.filter(order__boxer=boxer)\
+    @staticmethod
+    def get_course_base_info(dict, course):
+        dict['validity'] = course.validity
+        dict['boxer_id'] = course.boxer_id
+        dict['club_id'] = course.club.id
+        dict['club_name'] = course.club.name
+        dict['club_city'] = course.club.city
+        dict['club_address'] = course.club.address
+        dict['club_longitude'] = course.club.longitude
+        dict['club_latitude'] = course.club.latitude
+        return dict
+
+    @staticmethod
+    def get_boxer_base_info(dict, boxer):
+        dict['order_count'] = CourseOrder.objects.filter(boxer=boxer).count()
+        dict['allowed_course'] = boxer.allowed_course
+        comment_count_and_avg_score = OrderComment.objects.filter(order__boxer=boxer) \
             .aggregate(comments_count=Count('id'), avg_score=Avg('score'))
-        base_info.update(comment_count_and_avg_score)
-        base_info['order_count'] = CourseOrder.objects.filter(boxer=boxer).count()
-        base_info['allowed_course'] = boxer.allowed_course
-        if course:
-            base_info['validity'] = course.validity
-            base_info['boxer_id'] = course.boxer_id
-            if course.club:
-                base_info['club_id'] = course.club.id
-                base_info['club_name'] = course.club.name
-                base_info['club_city'] = course.club.city
-                base_info['club_address'] = course.club.address
-                base_info['club_longitude'] = course.club.longitude
-                base_info['club_latitude'] = course.club.latitude
-        return base_info
+        dict.update(comment_count_and_avg_score)
+        return dict
+
 
 
 class GetBoxerCourseByAnyOneViewSet(viewsets.ReadOnlyModelViewSet):
@@ -80,9 +83,9 @@ class GetBoxerCourseByAnyOneViewSet(viewsets.ReadOnlyModelViewSet):
             .select_related('club', 'boxer')
 
     def opened_courses_list(self, request, *args, **kwargs):
-        boxer = BoxerIdentification.objects.get(id=kwargs['boxer_id'])
-        course = self.get_queryset().filter(is_open=True).last()
         response = super().list(request, *args, **kwargs)
-        base_info = BoxerMyCourseViewSet.get_base_info(course=course, boxer=boxer)
-        response.data.update(base_info)
+        boxer = BoxerIdentification.objects.get(id=kwargs['boxer_id'])
+        course = self.get_queryset().select_related('club').last()
+        BoxerMyCourseViewSet.get_course_base_info(dict=response.data, course=course)
+        BoxerMyCourseViewSet.get_boxer_base_info(dict=response.data, boxer=boxer)
         return response
--- boxing_console/views/club.py
+++ boxing_console/views/club.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, filters, status
 from rest_framework.response import Response
 
-from biz.models import BoxingClub, Course
+from biz import redis_client
+from biz.models import BoxingClub, Course, BoxerIdentification
 from boxing_console.serializers import BoxingClubSerializer
 
 
@@ -17,10 +18,34 @@ class BoxingClubVewSet(viewsets.ModelViewSet):
 
     def close_club(self):
         club = self.get_object()
-        club.soft_delete()
-        Course.all_objects.filter(club=club.id).update(is_open=False)
+        self.del_club_and_location(club)
+        self.del_club_boxer_location(club)
+        self.close_club_courses(club)
         return Response({"message": "拳馆已关闭"}, status=status.HTTP_204_NO_CONTENT)
 
     def open_club(self):
-        BoxingClub.all_objects.filter(id=self.kwargs['pk']).update(is_deleted=False)
+        club = BoxingClub.all_objects.get(id=self.kwargs['pk'])
+        self.open_club_and_record_location(club)
         return Response({"message": "拳馆已开启"}, status=status.HTTP_204_NO_CONTENT)
+
+    @staticmethod
+    def del_club_and_location(club):
+        club.soft_delete()
+        redis_client.del_object_location(club)
+
+    @staticmethod
+    def open_club_and_record_location(club):
+        club.is_deleted = False
+        club.save()
+        redis_client.record_object_location(club, club.longitude, club.latitude)
+
+    @staticmethod
+    def del_club_boxer_location(club):
+        courses = Course.objects.filter(club=club.id).select_related('boxer')
+        for course in courses:
+            redis_client.del_object_location(course.boxer)
+
+    @staticmethod
+    def close_club_courses(club):
+        Course.objects.filter(club=club.id).update(is_open=False)
+
