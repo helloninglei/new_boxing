51758e8255d2053d5f575d43d9ba5cb4e5af3112
Lerry <lvdachao@tuotiansudai.com>
2018-06-15 15:10:39 +0800

    update deploy shell

--- service.sh
+++ service.sh
@@ -12,31 +12,42 @@ pull(){
     git pull origin master
 }
 
-init(){
-    build
+api(){
     docker run -p 5000:8000 --name new_boxing_app -v $PROJECT_PATH:/work -v $LOG_PATH:/var/log/boxing -e APP='boxing_app' -d -it new_boxing_image /bin/bash /work/deploy/run.sh
+}
+
+console(){
     docker run -p 5001:8000 --name new_boxing_console -v $PROJECT_PATH:/work -v $LOG_PATH:/var/log/boxing -e APP='boxing_console' -d -it new_boxing_image /bin/bash /work/deploy/run.sh
 }
 
+filter(){
+    echo $(docker ps --filter "name=new_boxing" --all --quiet)
+}
+
 reset(){
-    docker stop $(docker ps --filter "name=new_boxing" --all --quiet)
-    docker rm $(docker ps --filter "name=new_boxing" --all --quiet)
+    docker stop $(filter)
+    docker rm $(filter)
 }
 
-start(){
-    docker start $(docker ps --filter "name=new_boxing" --all --quiet)
-}
-
-stop(){
-    docker stop $(docker ps --filter "name=new_boxing" --all --quiet)
-}
 
 restart(){
-    docker restart $(docker ps --filter "name=new_boxing" --all --quiet)
+    docker restart $(filter)
 }
 
+init(){
+    if [ -z $2 ]; then
+        eval $2
+    else
+        api && console
+    fi
+}
 
 deploy(){
-    pull && restart
+    if [ ! "$(filter)" ]; then
+        build  && init
+    else
+        restart
+    fi
 }
+
 eval $1
