0cbbab6a09783f3952284f5f3973473900c0a7eb
Lerry <lvdachao@tuotiansudai.com>
2018-06-15 17:41:31 +0800

    update log path

--- deploy/config.py
+++ deploy/config.py
@@ -1,3 +1,4 @@
+import os
 import multiprocessing
 
 bind = "0.0.0.0:8000"
@@ -124,10 +125,10 @@ tmp_upload_dir = None
 #
 #       A string of "debug", "info", "warning", "error", "critical"
 #
-
-errorlog = '/work/logs/api_error.log'
+app = os.environ.get('APP')
+errorlog = f'/var/log/new_boxing/{app}_error.log'
 loglevel = 'debug'
-accesslog = '/work/logs/api_access.log'
+accesslog = f'/var/log/new_boxing/{app}_access.log'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 #
