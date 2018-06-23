766d36ebb0f7a3fdd27e2786b7870f2dc57d3270
fzw <fangzhaowei@tuotiansudai.com>
2018-06-23 09:45:45 +0800

    fix bug

--- biz/utils.py
+++ biz/utils.py
@@ -43,6 +43,8 @@ def get_video_cover_url(url):
 
 def hans_to_initial(hans):
     """返回中文词组第一个汉字的首字母"""
+    if not hans:
+        return ""
     first_hans = hans[0]
     first_letter = pinyin(first_hans, style=Style.FIRST_LETTER)
     return first_letter[0][0].upper()
