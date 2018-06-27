e3fc7bf94099aac70dcc4f0164551d1aac451d87
Lerry <lvdachao@tuotiansudai.com>
2018-06-27 15:12:28 +0800

    fix push_news test

--- boxing_console/tests/test_game_news.py
+++ boxing_console/tests/test_game_news.py
@@ -40,6 +40,7 @@ class GameNewsTestCase(APITestCase):
 
     def test_created_failed(self):
         self.data['start_time'] = datetime.now() - timedelta(days=1)
+        self.data['push_news'] = True
         res = self.client.post('/game_news', self.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(res.data['message'][0], '开始时间必须是以后的时间')
