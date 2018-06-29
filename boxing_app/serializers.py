cfd16391e28f3e2e2b0d141d2bcf93c1060860e2
ninglei <zhangninglei@tuotiansudai.com>
2018-06-29 11:34:28 +0800

    blank line

--- boxing_app/serializers.py
+++ boxing_app/serializers.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import re
 from datetime import datetime
-from django.utils import timezone
 
+from django.utils import timezone
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.transaction import atomic
