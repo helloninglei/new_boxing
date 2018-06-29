102001542add31e680baa79d6231f7980140798f
fzw <fangzhaowei@tuotiansudai.com>
2018-06-29 11:04:51 +0800

    fix bug

--- biz/services/money_balance_service.py
+++ biz/services/money_balance_service.py
@@ -13,7 +13,7 @@ def change_money(user, amount, change_type, remarks=None):
         user=user, change_type=change_type, last_amount=user.money_balance, change_amount=amount,
         remain_amount=user.money_balance + amount, operator=user, remarks=remarks
     )
-    rows = User.objects.filter(pk=user.id, money_balance__gt=-amount).update(money_balance=F('money_balance') + amount)
+    rows = User.objects.filter(pk=user.id, money_balance__gte=-amount).update(money_balance=F('money_balance') + amount)
     if rows == 0:
         raise ChangeMoneyException('余额不足')
     return money_change_log
