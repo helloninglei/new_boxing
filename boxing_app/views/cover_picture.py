496736f33152284f98a2bf54233eaaeb088b73d3
fzw <fangzhaowei@tuotiansudai.com>
2018-07-12 19:36:38 +0800

    cover picture

---
+++ boxing_app/views/cover_picture.py
@@ -0,0 +1,21 @@
+import subprocess as sp
+
+from django.http import StreamingHttpResponse
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+FFMPEG_BIN = "ffmpeg"
+
+
+@api_view(['GET'])
+@permission_classes([])
+@authentication_classes([])
+def cover_picture(request):
+    # doc: https://www.ffmpeg.org/ffmpeg.html
+    command = [FFMPEG_BIN,
+               '-ss', '0',
+               "-i", f'{request.query_params.get("video_url")}',
+               '-t', '1',
+               '-f', 'image2pipe',
+               '-']
+    pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8)
+    return StreamingHttpResponse(pipe.stdout, content_type="image/jpeg")
