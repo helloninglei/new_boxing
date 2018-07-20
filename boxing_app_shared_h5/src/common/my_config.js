3104d31308c9d1339592707241bc01e2cf950428
Lerry <lvdachao@tuotiansudai.com>
2018-07-20 22:53:48 +0800

    fix url

--- boxing_app_shared_h5/src/common/my_config.js
+++ boxing_app_shared_h5/src/common/my_config.js
@@ -1,6 +1,7 @@
 const config = {
     // 'baseUrl': location.host.indexOf('api') == 0 ? 'https://api.bituquanguan.com' : 'http://qa.bituquanguan.com',
-    'baseUrl': location.pathname == '/share/' ? location.protocol+'//'+location.host :'http://qa.bituquanguan.com',
+    // 'baseUrl': location.pathname == '/share/' ? location.protocol+'//'+location.host :'http://qa.bituquanguan.com',
+    'baseUrl': 'https://api.bituquanguan.com',
 };
 
 export default config;
