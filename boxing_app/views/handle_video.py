a2bf3525e18227c7973d126358566878422c33f0
fangzw <zwfangio@hotmail.com>
2018-08-31 18:42:11 +0800

    get video width height

--- boxing_app/views/handle_video.py
+++ boxing_app/views/handle_video.py
@@ -1,6 +1,7 @@
 import subprocess as sp
 from rest_framework.response import Response
 from rest_framework import status
+import re
 
 from django.http import StreamingHttpResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
@@ -25,3 +26,21 @@ def cover_picture(request):
     if pipe.returncode is not 0:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     return StreamingHttpResponse(pipe.stdout, content_type="image/jpeg")
+
+
+@api_view(['POST'])
+@permission_classes([])
+@authentication_classes([])
+def video_width_and_height(request):
+    pipe = sp.Popen(
+        ["ffprobe", "-v", "error", "-of", "flat=s=_", "-select_streams", "v:0", "-show_entries", "stream=height,width",
+         f"{request.data['video_url']}"], stdout=sp.PIPE)
+    pipe.wait()
+    if pipe.returncode is not 0:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    data = pipe.communicate()[0]
+
+    rex = re.compile(r"[^=]*=(\d+)[^=]*=(\d+).*")
+
+    width, height = rex.match(str(data)).groups()
+    return Response({"height": height, "width": width})
