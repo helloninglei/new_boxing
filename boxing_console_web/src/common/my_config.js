1070e1bd299ff18c27db7ec37645ed46f33b7596
sukankan <sukankan@tuotiansudai.com>
2018-09-02 23:39:09 +0800

    reset my_conf

--- boxing_console_web/src/common/my_config.js
+++ boxing_console_web/src/common/my_config.js
@@ -1,10 +1,5 @@
-/*
 let config = {
     'baseUrl': location.pathname == '/console/' ? location.protocol+'//'+location.host: 'http://qa.bituquanguan.com:8000',
-};
-*/
-let config = {
-    'baseUrl': location.pathname == '/console/' ? location.protocol+'//'+location.host: 'http://127.0.0.1:8000',
 };
 
 export default config;
