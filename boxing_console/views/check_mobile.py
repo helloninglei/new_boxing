2e6b1e70c1115ff14109d697a14a6df216ddca0e
ninglei <zhangninglei@tuotiansudai.com>
2018-09-13 14:12:57 +0800

    opt

--- boxing_console/views/check_mobile.py
+++ boxing_console/views/check_mobile.py
@@ -9,8 +9,8 @@ from biz.models import User, Player
 def check_player_mobile(request):
     mobile = request.query_params.get("mobile")
     is_player = Player.objects.filter(mobile=mobile).exists()
-    is_user = User.objects.filter(mobile=mobile).exists()
+    user = User.objects.filter(mobile=mobile).first()
     avatar = None
-    if is_user:
-        avatar = User.objects.get(mobile=mobile).user_profile.avatar
-    return Response(data={"is_user": is_user, "is_player": is_player, "avatar": avatar}, status=status.HTTP_200_OK)
+    if user:
+        avatar = user.user_profile.avatar
+    return Response(data={"is_user": bool(user), "is_player": is_player, "avatar": avatar}, status=status.HTTP_200_OK)
