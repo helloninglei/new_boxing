1fca67dca464debe3d4eae2e89ccd4c921d33108
ninglei <zhangninglei@tuotiansudai.com>
2018-09-13 14:08:38 +0800

    add permession

--- boxing_console/views/check_mobile.py
+++ boxing_console/views/check_mobile.py
@@ -1,13 +1,11 @@
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from biz.models import User, Player
 
 
 @api_view(['GET'])
-@permission_classes([])
-@authentication_classes([])
 def check_player_mobile(request):
     mobile = request.query_params.get("mobile")
     is_player = Player.objects.filter(mobile=mobile).exists()
