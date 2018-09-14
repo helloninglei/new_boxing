a0c6f0d742869815a5e6096f74b9db137d71235c
sukankan <sukankan@tuotiansudai.com>
2018-09-14 17:52:02 +0800

    fix UT

--- boxing_app/tests/test_ability.py
+++ boxing_app/tests/test_ability.py
@@ -8,7 +8,7 @@ class AbilityTest(APITestCase):
         self.user = User.objects.create_user(mobile='11111111111', password='password')
 
     def test_ability_chart(self):
-        res = self.client.get('/players/{}/ability'.format(self.user.id))
+        res = self.client.get('/players/{}/ability_chart'.format(self.user.id))
         self.assertEqual(res.status_code, 404)
         self.player = Player.objects.create(user_id=self.user.id, name='膜法师', mobile='11111111111',
                                             avatar='/path/to/face.jpg', stamina=50, skill=50, attack=50,
