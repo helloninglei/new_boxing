fb934d9755c1f3277817bddc96b41c6645bddc53
sukankan <sukankan@tuotiansudai.com>
2018-09-18 19:32:55 +0800

    fix UT v8

--- boxing_app/tests/test_share.py
+++ boxing_app/tests/test_share.py
@@ -100,7 +100,7 @@ class ShareTestCase(APITestCase):
         self.assertEqual(data['title'], '分享lerry的个人战绩')
         self.assertEqual(data['sub_title'], "已关注: 0,粉丝数: 0")
         self.assertEqual(data['picture'], f'{oss_base_url}{self.test_player.avatar}{img_prefix}')
-        self.assertEqual(data['url'], f'{h5_base_url}players/{self.test_user.id}')
+        self.assertEqual(data['url'], f'{h5_base_url}players/{self.test_player.id}')
 
     def test_title_content(self):
         self.news_data['title'] = '1' * 14
