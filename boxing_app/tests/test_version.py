cdfa85d911c1aaa25e4897e9a7dbab2fb3f7de76
sukankan <sukankan@tuotiansudai.com>
2018-09-21 22:48:30 +0800

    fix UT

--- boxing_app/tests/test_version.py
+++ boxing_app/tests/test_version.py
@@ -9,8 +9,9 @@ class VersionTestCase(APITestCase):
         self.user = User.objects.create_user(mobile="19909999999", password="password")
         self.client = self.client_class()
         self.client.login(username=self.user.mobile, password="password")
-        self.android_version = AppVersion.objects.get(platform=ANDROID, status=APPVERSION_NOW)
-        self.ios_version = AppVersion.objects.get(platform=IOS, status=APPVERSION_NOW)
+        self.android_version = AppVersion.objects.create(version='3.3.1', platform=ANDROID, status=APPVERSION_NOW,
+                                                         message='android-3-3-1', inner_number=1, force=True, package='/path/to/app.apk')
+        self.ios_version = AppVersion.objects.create(version='3.3.1', platform=IOS, status=APPVERSION_NOW,  force=True)
         self.android_data = {'version': self.android_version.version,
                              'message': self.android_version.message, 'force': self.android_version.force}
         self.ios_data = {'version': self.ios_version.version,
