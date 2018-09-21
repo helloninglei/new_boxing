db6a49caf075072ff52d57bdfc1d3944877033b4
sukankan <sukankan@tuotiansudai.com>
2018-09-21 18:37:24 +0800

    fix relation test

--- boxing_console/tests/test_app_version.py
+++ boxing_console/tests/test_app_version.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 from rest_framework.test import APITestCase
 from biz.models import User, AppVersion
-from biz.constants import APPVERSION_FUTURE, IOS, ANDROID
+from biz.constants import APPVERSION_FUTURE, IOS, ANDROID, VERSION_MANAGER_GROUP
 
 
 class AppVersionTest(APITestCase):
     def setUp(self):
-        self.user = User.objects.create_user(id=10158, mobile='10187654321', password='p@sSvV0rd',
+        self.user = User.objects.create_user(id=VERSION_MANAGER_GROUP[0], mobile='10187654321', password='p@sSvV0rd',
                                              is_staff=True, is_active=True, is_superuser=True)
         self.client = self.client_class()
         self.client.login(username=self.user, password='p@sSvV0rd')
