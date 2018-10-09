360f88f0e47f0f73bc20cea6c2cde9982791750d
zhaozhen <zhaozhen@tuotiansudai.com>
2018-10-09 14:30:21 +0800

    fix bugs

--- boxing_app_shared_h5/src/page/download.vue
+++ boxing_app_shared_h5/src/page/download.vue
@@ -106,7 +106,7 @@
                 if (this.page === 'hot_videos') {
                     location.href = `boxing://api.bituquanguan.com:80/${this.page}?id=${this.id}&userId=${this.userId}&time=${new Date().getTime()}`;
                 }
-                else if (this.page = 'home_page_match') {
+                else if (this.page === 'home_page_match') {
                     location.href = `boxing://api.bituquanguan.com:80/${this.page}?userId=${this.id}&time=${new Date().getTime()}`;
                 }
                 else {
