a1511edd795119e1288b2ae8cd09459a821d1c4b
ninglei <zhangninglei@tuotiansudai.com>
2018-10-23 12:27:41 +0800

    fix

--- biz/services/url_service.py
+++ biz/services/url_service.py
@@ -1,11 +1,6 @@
-from urllib.parse import urlparse
 
 from django.conf import settings
 
 
-def get_path(url):
-    return urlparse(url).path if url.startswith("http") else url
-
-
 def get_cdn_url(url):
-    return f"{settings.CDN_BASE_URL}{get_path(url)}"
+    return url if url.startswith("http") else f"{settings.CDN_BASE_URL}{url}"
